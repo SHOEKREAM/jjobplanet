fd4f43c5b6173f3c5f34a64607975ee530a56e18
dudrbs703 <dudrbs703@naver.com>
2022-02-16 15:32:08 +0900

    검색 내역 일부 추가

--- jjobplanet-spring/src/main/java/com/jjobplanet/jjobplanet/controller/HomeController.java
+++ jjobplanet-spring/src/main/java/com/jjobplanet/jjobplanet/controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.jjobplanet.jjobplanet.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -29,8 +37,15 @@ public class HomeController {
 		return "recruit";
 	}
 	
-	@GetMapping("/search")
-	public String search() {
+	@RequestMapping(value = "/search.do", method = RequestMethod.GET)
+	public String search(HttpServletRequest request) {
+		
+		// TODO: 검색 이력
+		String q = request.getParameter("q");
+		Cookie cookie = new Cookie("search_history", q);
+		
+		cookie.setMaxAge(0);
+		cookie.setPath("/");
 		return "search";
 	}
 
